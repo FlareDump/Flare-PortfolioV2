86b56df531c3e34cde5623845829a7e250c81efa
Flare <rayverdasalla@gmail.com>
2025-08-31 21:20:19 +0800

    Partial Finish

--- resources/views/Components/hero.blade.php
+++ resources/views/Components/hero.blade.php
@@ -18,18 +18,17 @@
         <div
             class="flex w-full flex-col-reverse justify-between gap-2 sm:py-2 md:flex-col-reverse md:gap-16 lg:flex-row lg:gap-16">
             <div class="hide-left flex flex-1 flex-col justify-center space-y-5">
-                <p class="font-primary md:ml-30 text-[var(--color-textPrimary)]/50 text-lg font-semibold lg:ml-0">
+                <p class="font-primary md:ml-30 text-textPrimary/50 text-lg font-semibold lg:ml-0">
                     <i>Hi, I'm</i>
                 </p>
                 <h1
-                    class="font-primary md:ml-30 text-left text-6xl font-black text-[var(--color-textPrimary)] md:text-7xl lg:ml-0 lg:text-8xl">
+                    class="font-primary md:ml-30 text-textPrimary text-left text-6xl font-black md:text-7xl lg:ml-0 lg:text-8xl">
                     RAYVER </h1>
                 <h1
-                    class="font-primary md:mr-30 text-right text-6xl font-black text-[var(--color-textPrimary)] md:text-7xl lg:mr-0 lg:text-8xl">
+                    class="font-primary md:mr-30 text-textPrimary text-right text-6xl font-black md:text-7xl lg:mr-0 lg:text-8xl">
                     DASALLA
                 </h1>
-                <p
-                    class="font-primary md:mr-30 text-[var(--color-textPrimary)]/50 text-end text-lg font-semibold lg:mr-0">
+                <p class="font-primary md:mr-30 text-textPrimary/50 text-end text-lg font-semibold lg:mr-0">
                     <i>(Flare)</i>
                 </p>
             </div>
